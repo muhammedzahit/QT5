a1b3405ff146234d533895e1ca85efa81f06461b
muhammedzahit <muhammetzahitaydin@gmail.com>
2020-09-25 22:39:30 +0300

    A basic login form with PyQt

---
+++ PyQt5- Login Form/turkish.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr" sourcelanguage="en">
+<context>
+    <name>Login</name>
+    <message>
+        <location filename="test.py" line="255"/>
+        <source>Logged in</source>
+        <translation type="unfinished">Giriş yapıldı</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="256"/>
+        <source>Login succesfull :)</source>
+        <translation type="unfinished">Başarılı giriş :)</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="262"/>
+        <source>Username and pasword didn&apos;t match !!!</source>
+        <translation type="unfinished">kullanıcı_adı ve şifre eşleşmiyor !!!</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="264"/>
+        <source>Login Error</source>
+        <translation type="unfinished">Giriş Hatası</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="test.py" line="193"/>
+        <source>MainWindow</source>
+        <translation type="unfinished">AnaPencere</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="194"/>
+        <source>Username: </source>
+        <translation type="unfinished">kullanıcı_adı: </translation>
+    </message>
+    <message>
+        <location filename="test.py" line="195"/>
+        <source>Password:</source>
+        <translation type="unfinished">şifre:</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="196"/>
+        <source>Confirm Password:</source>
+        <translation type="unfinished">şifre kontrol:</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="199"/>
+        <source>register</source>
+        <translation type="unfinished">Kaydol</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="198"/>
+        <source>login</source>
+        <translation type="unfinished">Giriş</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="200"/>
+        <source>USERNAME :</source>
+        <translation type="unfinished">kullanıcı_adı :</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="201"/>
+        <source>PASSWORD :</source>
+        <translation type="unfinished">şifre :</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="221"/>
+        <source>You must fill all lines !!!</source>
+        <translation type="unfinished">Bütün alanları doldurmalısınız !!!</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="226"/>
+        <source>Passwords didn&apos;t match !!!</source>
+        <translation type="unfinished">Şifreler eşleşmiyor !!!</translation>
+    </message>
+    <message>
+        <location filename="test.py" line="231"/>
+        <source>Username already taken !!!</source>
+        <translation type="unfinished">Bu kullanıcı adı alındı !!!</translation>
+    </message>
+</context>
+</TS>
